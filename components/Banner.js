21e1f4aa8855d759e4bed4e0c9148acf1763f652
pbvansh <vanshpratik0165@gmail.com>
2022-07-08 14:38:42 +0530

    done with header

---
+++ components/Banner.js
@@ -0,0 +1,11 @@
+
+
+const Banner = () => {
+  return (
+    <div>
+      <p>i am a Banner</p>
+    </div>
+  )
+}
+
+export default Banner
---
+++ components/Header.js
@@ -0,0 +1,99 @@
+import Image from "next/image"
+import { LocationMarkerIcon, MenuIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline'
+
+const Header = () => {
+  return (
+    <header className="sticky">
+      {/* top nav*/}
+      <div className="flex items-center bg-amazon_blue  py-2">
+        <div className=" relative flex flex-grow pt-2 mr-2 items-center sm:flex-grow-0">
+          <Image
+            className="cursor-pointer"
+            src={'https://links.papareact.com/f90'}
+            height={35}
+            width={140}
+            objectFit="contain" />
+          <span className="text-white absolute top-2 right-0">.in</span>
+        </div>
+        <div className="hidden lg:inline-flex text-white text-sm m-1 mr-4 whitespace-nowrap items-end link space-x-2">
+          <div>
+              <LocationMarkerIcon className="h-6"/>
+          </div>
+          <div>
+            <p>hello</p>
+            <p className="font-bold">Select your address</p>
+          </div>
+        </div>
+        <div className=" hidden sm:flex bg-yellow-400 rounded-md flex-grow items-center">
+          <div className="h-10 p-1 rounded-l-md bg-gray-100 hover:bg-gray-200  text-xs">
+            <select className=" h-full bg-transparent focus:outline-none">
+              <option>All</option>
+              <option>qwe</option>
+              <option>asd</option>
+              <option>ert</option>
+            </select>
+          </div>
+          <input type="text" className="outline-none flex-grow flex-shrink p-2" />
+          <SearchIcon className="h-5 w-5 mx-3" />
+        </div>
+
+        <div className="text-white text-sm flex space-x-6 whitespace-nowrap mx-4 items-center">
+          <div className="link">
+            <p> Hello pratik vansh</p>
+            <p className="font-bold md:text-sm"> Account & Lists</p>
+          </div>
+          <div className="link">
+            <p>
+              Returns
+            </p>
+            <p className="font-bold md:text-sm">& Orders</p>
+          </div >
+          <div className=" link flex items-center relative">
+            <span className="absolute flex top-0 -right-1 lg:right-10 h-4 w-4
+              bg-yellow-400 items-center justify-center rounded-full font-bold text-black ">0</span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden lg:inline font-bold md:text-sm mt-3">Basket</p>
+          </div>
+        </div>
+      </div>
+      {/* bottom nav */}
+
+      <div className="flex bg-amazon_blue-light whitespace-nowrap overflow-x-scroll scrollbar-hide text-white p-2 text-sm pl-5 items-center space-x-6">
+        <p className="flex link items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+      <p className="link">
+        Best Sellers
+      </p>
+      <p className="link">
+        Mobiles
+      </p>
+      <p className="link">
+        Customer Service
+      </p>
+      <p className="link">
+        Today's Deals
+      </p>
+      <p className="link">
+        Fashion
+      </p>
+      <p className="link">
+        Electronics
+      </p>
+      <p className="link">
+        Books
+      </p>
+      <p className="link">
+        Prime
+      </p>
+      <p className="link">
+        New Releases
+      </p>
+    </div>
+    </header >
+  )
+}
+
+export default Header
+//
