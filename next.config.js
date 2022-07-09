d873ce8b1a527aa7c20070f29f51ca84ac49d099
pbvansh <vanshpratik0165@gmail.com>
2022-07-09 14:34:53 +0530

    change

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains: ['links.papareact.com','fakestoreapi.com']
-  }
+  },
+  future: { webpack5: true }
 }
 
 module.exports = nextConfig
